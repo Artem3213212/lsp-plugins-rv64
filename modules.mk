09dea1e86f46c67ce6e67fed4038b581d5b40f74
Artem Gavrilov <37815717+Artem3213212@users.noreply.github.com>
2025-08-04 13:08:31 +0300

    Update modules.mk

--- modules.mk
+++ modules.mk
@@ -99,7 +99,7 @@ LSP_WS_LIB_URL_RO          := https://github.com/lsp-plugins/$(LSP_WS_LIB_NAME).
 LSP_WS_LIB_URL_RW          := git@github.com:lsp-plugins/$(LSP_WS_LIB_NAME).git
 
 # Plugin-related module dependencies
-LSP_PLUGIN_FW_VERSION      := 1.0.30
+LSP_PLUGIN_FW_VERSION      := master
 LSP_PLUGIN_FW_NAME         := lsp-plugin-fw
 LSP_PLUGIN_FW_TYPE         := src
 LSP_PLUGIN_FW_URL_RO       := https://github.com/Artem3213212/lsp-plugin-fw-rv64.git
